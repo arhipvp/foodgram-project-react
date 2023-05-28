c236c8fbaf4f3cc871c16214e85c605c95d68264
Архипов Владимир <arhipvp@gmail.com>
2023-05-28 16:17:35 +0300

    dd

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -30,7 +30,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
     'rest_framework',
     'rest_framework.authtoken',
     'djoser',
